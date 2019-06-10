9ff0988a0b78ca95d165c507fda20764468a09ec
Dylan Siegers <dsiegers@singularity3.cs.tamu.edu>
2019-06-10 11:10:12 -0500

    increased printk test cases for mempolicy.c - must be removed later

--- mm/mempolicy.c
+++ mm/mempolicy.c
@@ -2017,6 +2017,8 @@ alloc_pages_vma(gfp_t gfp, int order, struct vm_area_struct *vma,
 
 	pol = get_vma_policy(vma, addr);
 
+	printk("kernel print test line 2020 - mempolicy.c");
+
 	if (pol->mode == MPOL_INTERLEAVE) {
 		unsigned nid;
 
@@ -2487,6 +2489,8 @@ void mpol_shared_policy_init(struct shared_policy *sp, struct mempolicy *mpol)
 	sp->root = RB_ROOT;		/* empty tree == default mempolicy */
 	rwlock_init(&sp->lock);
 
+	printk("kernel print test line 2492 - mempolicy.c");
+
 	if (mpol) {
 		struct vm_area_struct pvma;
 		struct mempolicy *new;
@@ -2835,6 +2839,8 @@ void mpol_to_str(char *buffer, int maxlen, struct mempolicy *pol)
 	nodemask_t nodes = NODE_MASK_NONE;
 	unsigned short mode = MPOL_DEFAULT;
 	unsigned short flags = 0;
+
+	printk("kernel print test line 2843 - mempolicy.c");
 
 	if (pol && pol != &default_policy && !(pol->flags & MPOL_F_MORON)) {
 		mode = pol->mode;
